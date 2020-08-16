094e47dd890372684a3d14dc6f8cb48dfd9d8729
vipulsingh270 <vipul.singh270@gmail.com>
2020-08-16 17:44:41 +0530

    Code commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/17/8094631656de001a1316e160c15993d0
@@ -0,0 +1,5 @@
+package com.hr.entity;
+
+public class Employee {
+
+}
---
+++ javaee-api/src/main/java/com/hr/service/PersistenceService.java
@@ -0,0 +1,84 @@
+package com.hr.service;
+
+import java.util.Collection;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import com.hr.entity.Employee;
+import com.hr.entity.Team;
+
+@Transactional
+public class PersistenceService {
+	
+	@PersistenceContext
+	EntityManager em;
+	
+	public void saveTeam(Team team) {		
+		em.persist(team);
+	}
+	
+	public void saveEmployee(Employee employee) {
+		em.persist(employee);		
+	}
+	
+	public void assignTeam(Employee employee) {		
+		em.merge(employee);		
+	}
+	
+	public Employee getEmployeebyId(Long id) {
+		return em.find(Employee.class, id);
+	}
+	
+	public Team getTeambyId(Long id) {
+		return em.find(Team.class, id);
+	}
+	
+	public Collection<Employee> getAllEmployees(){
+		
+		return em.createNamedQuery("ListEmployees", Employee.class).getResultList();
+		
+	}
+	
+	public void removeEmployee(Long id) {
+		Employee emp = getEmployeebyId(id);
+		em.remove(emp);		
+	}
+	
+	public boolean isTeamExists(Team team) {
+		try {
+		Team teamd = em.createNamedQuery("findByTeamName", Team.class).setParameter("name", team.getName().toUpperCase()).getSingleResult();
+		if(teamd.equals(team)) {
+			return true;
+		}
+		else
+			return false;
+		}
+		catch(NoResultException e) {
+			return false;
+		}		
+	}
+	
+	public boolean isEmployeeExists(Employee emp) {
+		try {
+			Employee e = em.find(Employee.class,emp.getEmployeeId());
+			if(e != null)
+				return true;
+			else
+				return false;
+		}
+		catch(NoResultException e) {
+			return false;
+		}
+	}
+	
+	public Collection<Team> getAllTeams(){
+		return em.createNamedQuery("getAllTeams", Team.class).getResultList();
+	}
+	
+	public Team getTeambyName(String name) {
+		return em.createNamedQuery("findByTeamName", Team.class).setParameter("name", name.toUpperCase()).getSingleResult();
+	}
+}
